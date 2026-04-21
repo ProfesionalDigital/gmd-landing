696c46592b703f4c879e935fd4568f42d5458d0c
ProfesionalDigital <profesionaldigital@impormedical.com.co>
2026-04-21 14:22:39 -0500

    Changing social media urls

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -31,12 +31,12 @@ export const Footer = () => {
 
             {/* Redes Sociales */}
             <div className="flex items-center gap-4 pt-2">
-              <a href="https://www.facebook.com/ImpormedicalHabilitandoVidas/" className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/80 hover:bg-brand-500 hover:text-white transition-all duration-300">
+              <a href="https://www.facebook.com/gmddiagnostic" className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/80 hover:bg-brand-500 hover:text-white transition-all duration-300">
                 <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
                 </svg>
               </a>
-              <a href="https://www.youtube.com/@impormedicalcolombia" className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/80 hover:bg-brand-500 hover:text-white transition-all duration-300">
+              <a href="https://www.youtube.com/@GMDDIAGNOSTICO" className="w-10 h-10 rounded-full bg-white/10 flex items-center justify-center text-white/80 hover:bg-brand-500 hover:text-white transition-all duration-300">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg>
